141cfb7cff9903c9e2c59cfcfdebfb035de323a7
Josiah King <josiahpjking@gmail.com>
2018-01-23 15:19:42 +0000

    editing plotting stuff

--- R/make_tcplot_data.R
+++ R/make_tcplot_data.R
@@ -1,5 +1,5 @@
 #' time course plots for eye tracking
-#' @param objects vector of string names of AOIs (for eyelink trackers, the RIGHT_1_P, RIGHT_2_P,... columns)
+#' @param AOIs vector of string names of AOIs (for eyelink trackers, the RIGHT_1_P, RIGHT_2_P,... columns)
 #' @param predictor variable name of experimental condition.
 #' @param bin BIN variable (for eyelink, this is the CURRENT_BIN column). Defaults to CURRENT_BIN.
 #' @param data dataset
@@ -7,13 +7,13 @@
 #' @examples
 #' plottingdata <- make_tcplot_data(df=e6data, objects=c("easy_fix","diff_fix","vid_fix"),predictor=gesture)
 
-make_tcplot_data<-function(df, objects, predictor, bin=CURRENT_BIN, bin_interval=20){
+make_tcplot_data<-function(df, AOIs, predictor, bin=CURRENT_BIN, bin_interval=20){
   require(tidyverse)
   require(lazyeval)
   predictor=eval(substitute(predictor),df)
   bin=eval(substitute(bin),df)
   plotdat<-as.data.frame(list())
-  for (i in objects){
+  for (i in AOIs){
     x<-aggregate(df[,i],by=list(bin, predictor),FUN=mean)
     x$se<-aggregate(df[,i],by=list(bin, predictor),FUN=plotrix::std.error)[,3]
     x$low<-x$x-x$se
@@ -21,9 +21,9 @@ make_tcplot_data<-function(df, objects, predictor, bin=CURRENT_BIN, bin_interval
     x$object<-i
     plotdat<-rbind(plotdat,x)
   }
-  names(plotdat)<-c("bin","condition","mean","se","low","up","object")
+  names(plotdat)<-c("bin","condition","mean_prop","se","low","up","AOI")
   plotdat$time<-plotdat$bin*bin_interval
-  plotdat$object<-factor(plotdat$object)
+  plotdat$AOI<-factor(plotdat$AOI)
   return(plotdat)
 }
 
--- R/tcplot.R
+++ R/tcplot.R
@@ -7,13 +7,16 @@
 #' @examples
 #' tcplot(plotting_data, -1000, 4000)
 #' tcplot(plotting_data, -1000, 4000)+facet_wrap(~condition)
-tcplot<-function(df,xmin=0,xmax=2000,lcol=condition,ltype=object){
-  lcol=eval(substitute(lcol),df)
+tcplot<-function(df,xmin=0,xmax=2000,lcol="darkred",ltype="solid"){
+  lcol=eval(substitute(lcol),df)  
   ltype=eval(substitute(ltype),df)
   
-  tplot = ggplot() + xlim(xmin, xmax) + ylim(0, 1) + 
+  tplot = ggplot(data = df, aes(x = time, y = mean_prop, colour=lcol, fill=lcol, lty=ltype))+
+    xlim(xmin, xmax) + ylim(0, 1) + 
     xlab("Time (ms)") + ylab("Proportion of fixations to AOIs.") + 
-    geom_line(data = df, aes(x = time, y = mean, color = lcol, lty=ltype), lwd = 1.5) + 
-    geom_ribbon(data = df, aes(x = time, ymin = low, ymax = up, fill = lcol, lty=ltype), color = NA, alpha = 0.2, lwd = 1.5)
+    geom_line(lwd = 1.5) + 
+    geom_ribbon(data = df, aes(x = time, ymin = low, ymax = up), colour = NA, alpha = 0.2, lwd = 1.5)
+
   return(tplot)
 }
+
