d719daa8038cbd9783f072530c7f7c2e5db86051
Josiah King <josiahpjking@gmail.com>
2018-01-29 14:01:42 +0000

    tidying plot

--- R/tcplot.R
+++ R/tcplot.R
@@ -1,22 +1,18 @@
 #' time course plots for eye tracking
 #' @param df a dataframe in long format from the make_tcplot_data function
 #' @param xmin,xmax min and max time boundaries. (ROI)
-#' @param lcol = line colours variable.
-#' @param ltype = line type variable.
+#' @param lcol = line colours variable, string.
+#' @param ltype = line type variable, string.
 #' @export
 #' @examples
 #' tcplot(plotting_data, -1000, 4000)
 #' tcplot(plotting_data, -1000, 4000)+facet_wrap(~condition)
-tcplot<-function(df,xmin=0,xmax=2000,lcol="darkred",ltype="solid"){
-  lcol=eval(substitute(lcol),df)  
-  ltype=eval(substitute(ltype),df)
-  
-  tplot = ggplot(data = df, aes(x = time, y = mean_prop, colour=lcol, fill=lcol, lty=ltype))+
+tcplot<-function(df,xmin=0,xmax=2000,lcol="AOI",ltype="condition"){
+  tplot = ggplot(data = df, aes_string(x = "time", y = "mean_prop", colour=lcol, fill=lcol, lty=ltype))+
     xlim(xmin, xmax) + ylim(0, 1) + 
     xlab("Time (ms)") + ylab("Proportion of fixations to AOIs.") + 
     geom_line(lwd = 1.5) + 
-    geom_ribbon(data = df, aes(x = time, ymin = low, ymax = up), colour = NA, alpha = 0.2, lwd = 1.5)
+    geom_ribbon(data = df, aes_string(x = "time", ymin = "low", ymax = "up"), colour = NA, alpha = 0.2, lwd = 1.5)
 
   return(tplot)
 }
-
