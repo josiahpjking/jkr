52c31202c9c313e7efff004a896ab00d639e9016
Josiah King <josiahpjking@gmail.com>
2018-03-11 13:38:55 +0000

    documented and added notin and lm.beta.lmer

--- R/lm.beta.lmer.R
+++ R/lm.beta.lmer.R
@@ -1,3 +1,8 @@
+#' equivalent of lm.beta for mixed models.
+#' @param mod linear mixed model object
+#' @export
+#' @examples
+#' lm.beta.lmer(fit1)
 lm.beta.lmer <- function(mod) {
   b <- fixef(mod)[-1]
   sd.x <- apply(getME(mod,"X")[,-1],2,sd)
--- R/notin.R
+++ R/notin.R
@@ -1,3 +1,9 @@
-`%notin%` = function(x,y){
+#' negation of %in% base function
+#' @param x the values to be matched
+#' @param y the values to be matched against
+#' @export
+#' @examples
+#' c(1,2,3) %notin% c(2,4,6) returns TRUE, FALSE, TRUE
+`%notin%` <- function(x,y){
   !(x %in% y)
 } 
