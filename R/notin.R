d767d4aa83ed901cb79c7966449092426139f5f1
Josiah King <josiahpjking@gmail.com>
2018-03-15 15:28:08 +0000

    gixing notin, can't use grapes

--- R/notin.R
+++ R/notin.R
@@ -3,7 +3,7 @@
 #' @param y the values to be matched against
 #' @export
 #' @examples
-#' 1:4 %notin% 2:5 returns TRUE, FALSE, FALSE, FALSE
-`%notin%` <- function(x,y){
+#' 1:4 notin 2:5 returns TRUE, FALSE, FALSE, FALSE
+notin <- function(x,y){
   !(x %in% y)
 } 
