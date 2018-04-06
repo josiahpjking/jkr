f6cbaccbf9958a83f0685bee51e37b0036c6ff1b
Josiah King <josiahpjking@gmail.com>
2018-04-06 12:16:23 +0100

    got rid of stupid notin function

--- R/notin.R
+++
@@ -1,9 +0,0 @@
-#' negation of %in% base function
-#' @param x the values to be matched
-#' @param y the values to be matched against
-#' @export
-#' @examples
-#' 1:4 %notin% 2:5 returns TRUE, FALSE, FALSE, FALSE
-"%notin%" <- function(x,y){
-  !(x %in% y)
-} 
