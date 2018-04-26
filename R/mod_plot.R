3a30d866811f316e09bb46561ff853b34dcd62d6
Josiah King <josiahpjking@gmail.com>
2018-04-26 16:01:18 +0100

    tidying up

--- R/mod_plot.R
+++
@@ -1,8 +0,0 @@
-mod_plot <- function(df, dv, iv, mdl){
-  new_plot <- df %>% mutate(
-    fitted = fitted(mdl)
-  ) %>% make_tcplot_data(., AOIs=c("fitted",dv),predictor=iv) %>% 
-    tcplot(.,xmin=min(.[,"time"]), xmax=(max(.[,"time"])),ltype="AOI",lcol=iv)+ylim(-2,2)
-  
-  return(new_plot)
-}
