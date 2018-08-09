589a09ea5dec97a2cf42d51492fdd07a86584faf
josiahpjking <josiahpjking@gmail.com>
2018-08-09 08:50:57 +0100

    update

---
+++ new_op_sys.R
@@ -0,0 +1,21 @@
+#agi libcurl4-openssl-dev libssl-dev libxml2-dev
+
+install.packages("curl")
+install.packages("RCurl")
+
+install.packages("tidyverse")
+install.packages("lubridate")
+
+install.packages("rsconnect")
+install.packages("devtools")
+
+install.packages("shiny")
+install.packages("shinydashboard")
+install.packages("plotly")
+
+install.packages("rmarkdown")
+install.packages("knitr")
+install.packages("caTools")
+
+install.packages("lme4")
+
