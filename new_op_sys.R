2417104baae70979e38f1d69b43b1240cbc04858
Josiah King <josiahpjking@gmail.com>
2019-04-09 08:53:57 +0100

    quick up

--- new_op_sys.R
+++ new_op_sys.R
@@ -1,21 +1,4 @@
-#agi libcurl4-openssl-dev libssl-dev libxml2-dev
-
-install.packages("curl")
-install.packages("RCurl")
-
-install.packages("tidyverse")
-install.packages("lubridate")
-
-install.packages("rsconnect")
-install.packages("devtools")
-
-install.packages("shiny")
-install.packages("shinydashboard")
-install.packages("plotly")
-
-install.packages("rmarkdown")
-install.packages("knitr")
-install.packages("caTools")
-
-install.packages("lme4")
+#sudo apt-get install r-base r-base-dev 
+#sudo apt-get install libcurl4-openssl-dev libssl-dev libxml2-dev
+install.packages(c("curl","RCurl","rsconnect","devtools","tidyverse","shiny","shinydashboard","plotly","rmarkdown","knitr","caTools","lme4","arm","rio"))
 
