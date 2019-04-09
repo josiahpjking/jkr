bee9e0fcab9a5a3aef1de05369d125de61dea893
Josiah King <josiahpjking@gmail.com>
2019-04-09 15:38:37 +0100

    Update new_op_sys.R

--- new_op_sys.R
+++ new_op_sys.R
@@ -1,6 +1,6 @@
 #sudo apt-get install r-base r-base-dev 
 #sudo apt-get install libcurl4-openssl-dev libssl-dev libxml2-dev
-install.packages(c("curl","RCurl","rsconnect","devtools","tidyverse","shiny","shinydashboard","plotly","rmarkdown","knitr","caTools","lme4","arm","rio"))
+install.packages(c("curl","RCurl","rsconnect","devtools","tidyverse","shiny","shinydashboard","plotly","rmarkdown","knitr","caTools","lme4","arm","rvest","here","rio"))
 
 #rio?
 #rio::install_formats()
