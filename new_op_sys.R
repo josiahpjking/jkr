73b29f930af18c2a7b1cbbd1caf47179d5ee8479
Josiah King <josiahpjking@gmail.com>
2019-04-11 08:32:07 +0100

    Update new_op_sys.R

--- new_op_sys.R
+++ new_op_sys.R
@@ -1,6 +1,24 @@
 #sudo apt-get install r-base r-base-dev 
 #sudo apt-get install libcurl4-openssl-dev libssl-dev libxml2-dev
-install.packages(c("curl","RCurl","rsconnect","devtools","tidyverse","shiny","shinydashboard","plotly","rmarkdown","knitr","caTools","lme4","arm","rvest","here","rio","mgcv","itsadug"))
+install.packages(c("curl","RCurl","rsconnect","devtools","tidyverse"))
 
-#rio?
+#rmarkdown stuff
+install.packages(c("rmarkdown","knitr","caTools"))
+
+#shiny/plotly etc.
+install.packages(c("shiny","shinydashboard","plotly"))
+
+#various things for data 
+install.packages(c("rvest","here","rio","jsonlite"))
 #rio::install_formats()
+
+#lmm things
+install.packages(c("lme4","arm","broom","nlme"))
+
+#gam things
+install.packages(c("mgcv","itsadug"))
+
+#zero inflation models?
+install.packages(c("pscl","glmmTMB","glmmAdaptive"))
+
+
