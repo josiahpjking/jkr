737ff8728b952257e7cde10d65e9d019c8630b5a
Josiah King <josiahpjking@gmail.com>
2019-08-21 10:20:03 +0100

    up

---
+++ R/BSmake_tcplotdata.R
@@ -0,0 +1,45 @@
+#' time course plots for eye tracking computes avg proportion and BOOTSTRAPPED by subject 95% CIs (for Dale).
+#' @param df dataframe
+#' @param AOIs column names of AOIs (for eyelink trackers, the RIGHT_1_P, RIGHT_2_P,... columns)
+#' @param subj participant column
+#' @param ... variable names of experimental conditions
+#' @param bin BIN variable (for eyelink, this is the CURRENT_BIN column). Defaults to "CURRENT_BIN".
+#' @param bin_interval the time of each bin
+#' @param n bootstrap samples. so.. R really..
+#' @export
+#' @examples
+#' plottingdata <- BSmake_tcplotdata(df,AOIs=c(refprop,disprop),subj=Participant,n=1000,fluency,gesture)
+#' tcplot(plottingdata,0,2000,lty=fluency)+facet_wrap(~gesture)
+BSmake_tcplotdata<-function(df,AOIs,subj,n=100,...,bin=CURRENT_BIN, bin_interval=20){
+  subj=enquo(subj)
+  bin=enquo(bin)
+  AOIs=enquo(AOIs)
+
+  summy <- function(x){x %>% group_by(AOI,...,!!bin) %>% summarise(smean=mean(prop))}
+
+  samples <- tibble(
+    subjsample=map(1:n, ~sample(distinct(df,!!subj) %>% pull(!!subj),replace=T)),
+    meanprop=map(subjsample,~map_dfr(.,~filter(df,!!subj==.x)) %>%
+                   gather(key="AOI",value="prop",!!AOIs) %>%
+                   summy)
+  ) %>%
+    pull(meanprop) %>%
+    plyr::ldply() %>%
+    group_by(AOI,...,!!bin) %>%
+    summarise(
+      bs_SE=qnorm(.975)*sd(smean)
+    ) %>%
+    left_join(.,
+              df %>%
+                gather(key="AOI",value="prop",!!AOIs) %>%
+                group_by(AOI,...,!!bin) %>%
+                summarise(
+                  mean_prop=mean(prop)
+                )
+    ) %>%
+    mutate(
+      low=mean_prop-bs_SE,
+      up=mean_prop+bs_SE,
+      time=!!bin*bin_interval
+    ) %>% ungroup
+}
--- bysubjerror.R
+++ bysubjerror.R
@@ -1,37 +1,47 @@
 require(tidyverse)
-e8_et<-readRDS("phd_working-master/phd_working-master/e7e8_gcd/e8_gcdv2/gcd_eyes.RDS") %>% filter(clicked!="none") %>%
+require(jkr)
+e8_et<-readRDS("Desktop/git_repositories/phd_working/e7e8_gcd/e8_gcdv2/gcd_eyes.RDS") %>% filter(clicked!="none") %>%
+  filter(!(sub %in% c(1,8,18))) %>%
   mutate(ref=referent,vid_type=fct_relevel(gesture,"No Gesture"))
 
 make_tcplotdata(e8_et, c(ref_fix,dis_fix,vid_fix),sub,gesture) %>%
   tcplot(lty=gesture)
 
 
-make_tcplotdata2(e8_et, c(ref_fix,dis_fix,vid_fix),sub,gesture) %>%
-  tcplot(lty=gesture)
-
-
-
 e8_et %>% filter(CURRENT_BIN<=100) -> e8_et
 
-make_tcplotdata2<-function(df,AOIs,subj,n=100,...,bin=CURRENT_BIN, bin_interval=20){
+df<-e8_et1
+
+summy = function(x) x %>% group_by(AOI,gesture,CURRENT_BIN) %>%
+  summarize(smean=mean(prop))
+
+tibble(
+  subjsample=future_map(1:10, ~sample(distinct(df,sub) %>% pull(sub),replace=T)),
+  data=map(subjsample,~map_dfr(.,~filter(df,sub==.x)) %>%
+             gather(key="AOI",value="prop",ref_fix,dis_fix,vid_fix) %>%
+             group_by(AOI,gesture,CURRENT_BIN) %>%
+             summarize(smean=mean(prop)))
+) %>% pull(data) %>% plyr::ldply()
+
+
+BSmake_tcplotdata<-function(df,AOIs,subj,n=100,...,bin=CURRENT_BIN, bin_interval=20){
   subj=enquo(subj)
   bin=enquo(bin)
   AOIs=enquo(AOIs)
-  
-  summy = function(x) x %>% group_by(AOI,...,!!bin) %>% 
-    summarize(smean=mean(prop))
-  
-  samples = tibble(
-      subjsample=map(1:n, ~sample(distinct(df,sub) %>% pull(sub),replace=T)),
-      data=map(subjsample,~map_dfr(.,~filter(df,sub==.x))),
-      tidydata=map(data,~gather(.,key="AOI",value="prop",!!AOIs)),
-      meanprop=map(tidydata,summy)
-  )
-  
-  samples$meanprop %>% plyr::ldply() %>%
+
+  summy <- function(x){x %>% group_by(AOI,...,!!bin) %>% summarise(smean=mean(prop))}
+
+  samples <- tibble(
+      subjsample=map(1:n, ~sample(distinct(df,!!subj) %>% pull(!!subj),replace=T)),
+      meanprop=map(subjsample,~map_dfr(.,~filter(df,!!subj==.x)) %>%
+                     gather(key="AOI",value="prop",!!AOIs) %>%
+                     summy)
+  ) %>%
+    pull(meanprop) %>%
+    plyr::ldply() %>%
     group_by(AOI,...,!!bin) %>%
     summarise(
-      bs_SE=sd(smean)
+      bs_SE=qnorm(.975)*sd(smean)
     ) %>%
     left_join(.,
               df %>%
@@ -44,16 +54,35 @@ make_tcplotdata2<-function(df,AOIs,subj,n=100,...,bin=CURRENT_BIN, bin_interval=
     mutate(
       low=mean_prop-bs_SE,
       up=mean_prop+bs_SE,
-      time=CURRENT_BIN*bin_interval
+      time=!!bin*bin_interval
     )
 }
 
 
-make_tcplotdata2(e8_et,c(ref_fix,dis_fix,vid_fix),sub,n=500,gesture) -> tc
-tcplot(tc,lty=gesture)
+e8_et1<-e8_et %>% filter(CURRENT_BIN<=10)
 
+system.time(make_tcplotdata2(e8_et,c(ref_fix,dis_fix,vid_fix),sub,n=50,gesture) -> tcf)
+tcf$meanprop
+tcplot(tc)+facet_wrap(~gesture)
+
+make_tcplotdata(e8_et,c(ref_fix,dis_fix,vid_fix),subj=sub,gesture) ->tc2
+tcplot()+facet_wrap(~gesture)
+
+
+left_join(tc2,tc %>% select(gesture,CURRENT_BIN,AOI,bs_SE)) %>% mutate(
+  high=ifelse(bs_SE>se,1,0),
+  low=ifelse(bs_SE<se,1,0)
+) %>%
+  ggplot(.,aes(x=se,y=bs_SE))+geom_point()
+
+
+tc2 %>% mutate(orse=se,ormp=mean_prop) %>% select(-low,-up,-mean_prop) %>%
+  left_join(.,tc) -> compd
+plot(compd$ormp,compd$mean_prop)
+plot(compd$orse,compd$bs_SE)
+
+BlandAltmanLeh::bland.altman.plot(compd$orse,compd$bs_SE)
+tcplot(tc2)+facet_wrap(~gesture)
 
 make_tcplotdata(e8_et,c(ref_fix,dis_fix,vid_fix),sub,gesture) -> tc2
-tcplot(tc2,lty=gesture)
-
 
