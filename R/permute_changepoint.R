1d405696c41b0630bbe94eaa39b7fdb348616d16
Josiah King <josiahpjking@gmail.com>
2019-09-24 11:19:52 +0100

    update changepoint

--- R/permute_changepoint.R
+++ R/permute_changepoint.R
@@ -1,13 +1,13 @@
 #' returns a permutation based distribution of possible changepoint values of X.
-#' fits gam with n knots, find changepoint, loop(shuffles residuals, fits new gam, find changepoint)
-#' 
+#' fits gam with n knots, find changepoint, loop(new gam for (fitted+shuffled residuals)~x, find changepoint)
+#'
 #' I guess if you want to do some hypothesis testing, then do a z test of your theorised changepoint & permuted distribution?
 #' or of your permuted dist vs prior dist?
-#' 
+#'
 #' requires mgcv, tidyverse
 #'
 #' @param df dataframe or tibble object
-#' @param y y variable 
+#' @param y y variable
 #' @param x x variable (numeric)
 #' @param npoints number of changepoints to look for
 #' @param iter number of permutations/iterations
@@ -17,7 +17,7 @@
 permute_changepoint<-function(df,y,x,npoints,iter){
   xvar = df %>% pull({{x}})
   yvar = df %>% pull({{y}})
-  
+
   #observed
   m1 = gam(yvar ~ s(xvar, bs="cs", k=npoints))
   #predict y based on length of x
@@ -25,8 +25,8 @@ permute_changepoint<-function(df,y,x,npoints,iter){
   #plot(diff(py))
   cp_obs = cumsum(rle(as.numeric(diff(py)>0))$lengths) #run along the length of py, and get change points (cumsum preserves x)
   cp_obs = cp_obs[-length(cp_obs)]
-  
-  
+
+
   cp_permute<-function(mod){
     perm_y=sample(resid(mod),replace=T)+fitted(mod)
     perm_m=gam(perm_y ~ s(xvar, bs="cs", k=npoints))
@@ -34,11 +34,11 @@ permute_changepoint<-function(df,y,x,npoints,iter){
     perm_cp=cumsum(rle(as.numeric(diff(perm_py)>0))$lengths)
     perm_cp[-length(perm_cp)]
   }
-  
+
   cp_dist = map(1:iter,~cp_permute(m1)) %>% unlist %>%
     map_dbl(.,~seq(min(xvar), max(xvar), length.out = length(xvar)*4)[.])
-  
-  
+
+
   cp_plot<-function(yv,xv,dist,mod){
     ggplot(mapping=aes(x=xv,y=yv))+geom_point()+
       geom_line(aes(y=fitted(mod)),col="slateblue")+
