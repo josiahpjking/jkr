e73784cd0dc07b137e1f4489592c33e022fd3088
Josiah King <josiahpjking@gmail.com>
2019-10-29 10:51:21 +0000

    up

--- new_op_sys.R
+++ new_op_sys.R
@@ -1,24 +1,37 @@
-#sudo apt-get install r-base r-base-dev 
+#sudo apt-get install r-base r-base-dev
 #sudo apt-get install libcurl4-openssl-dev libssl-dev libxml2-dev
-install.packages(c("curl","RCurl","rsconnect","devtools","tidyverse","conflicted"))
+install.packages(c("curl","RCurl","rsconnect","devtools","roxygen2","tidyverse","conflicted","readxl","rvest","jsonlite","httr","janitor","foreign","furrr","xml2"))
+
+instal.packages(c("lubridate","padr"))
+
+install.packages(c("gghighlight","gganimate","ggridges","cowplot","viridis"))
 
 #rmarkdown stuff
-install.packages(c("rmarkdown","knitr","caTools"))
+install.packages(c("rmarkdown","knitr","kableExtra","caTools","blogdown"))
 
 #shiny/plotly etc.
 install.packages(c("shiny","shinydashboard","plotly","leaflet"))
 
-#various things for data 
-install.packages(c("rvest","here","rio","jsonlite"))
-#rio::install_formats()
-
 #lmm things
-install.packages(c("lme4","arm","broom","nlme"))
+install.packages(c("lme4","arm","broom","nlme","blme"))
 
 #gam things
 install.packages(c("mgcv","itsadug"))
 
-#zero inflation models?
-install.packages(c("pscl","glmmTMB","glmmAdaptive"))
+#nnets and multinoms
+install.packages("neuralnet")
+
+#bayes
+install.packages(c("rstan","rstanarm","tidybayes","rethinking"))
+
+#audio
+install.packages(c("tuneR"))
+
+#spatial etc
+install.packages(c("spdep"))
 
 
+devtools::install_github("josiahpjking/jkr")
+devtools::install_github("josiahpjking/DasGuptR")
+devtools::install_github("dalejbarr/clusterperm")
+devtools::install_github("dalejbarr/exchangr")
